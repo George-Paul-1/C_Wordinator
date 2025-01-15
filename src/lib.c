f2ed41f975456dd26105c01c7a91dcc2c0a60b30
George-Paul-1 <george.paul5236@gmail.com>
2025-01-15 16:57:42 +0000

    first commit

--- src/lib.c
+++ src/lib.c
@@ -1,3 +1,87 @@
-int add(int a, int b) {
-  return a + b;
+#include <stdbool.h>
+
+int processHundredThousands(int n, char* res, char* ones[], char* tens[], char* teens[]) { 
+  if (n >= 100000) {
+    strcat(res, ones[n / 100000]);
+    strcat(res, " hundred");
+    n = n % 100000;
+    if (n > 0) {
+      strcat(res, " and ");
+    } else {
+      strcat(res, " thousand");
+    }
+  } 
+  if (n >= 10000) {
+      if (n % 10000 >= 1000 && n < 20000) {
+        strcat(res, teens[n / 10000]);
+      } else {
+        strcat(res, tens[n / 10000]);
+      }
+      if (n % 10000 >= 1000 && n > 20000) {
+        strcat(res, " ");
+        strcat(res, ones[(n % 10000) / 1000]);
+      }
+      strcat(res, " thousand");
+      n = n % 1000;
+      if (n > 99) {
+        strcat(res, " ");
+      } else if (n > 0) {
+        strcat(res, " and ");
+      }
+    }
+  return n;
+}
+
+void wordinator(int n, char* res) {
+  char *ones[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+  char *tens[] = {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+  char *teens[] = {"", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+
+  if (n == 0) {
+    strcpy(res, "zero");
+    return;
+  }
+
+  if (n >= 10000) {
+    n = processHundredThousands(n, res, ones, tens, teens);
+  }
+
+  if (n >= 1000) {      
+      strcat(res, ones[n / 1000]);
+      strcat(res, " thousand");
+      n = n % 1000;
+    
+    if (n > 99) {
+      strcat(res, " ");
+    } else if (n > 0) {
+      strcat(res, " and ");
+    }
+  }
+  
+  if (n >= 100) {
+    strcat(res, ones[n / 100]);
+    strcat(res, " hundred");
+    n = n % 100;
+    if (n > 0) {
+      strcat(res, " and ");
+    }
+  }
+  
+  if (n == 0) {
+    return;
+  }
+
+  if (n >= 20 || n == 10) {
+    strcat(res, tens[n/10]);
+    n = n % 10;
+    if (n > 0) {
+      strcat(res, " ");
+    }
+  }
+  
+  if (n / 10 > 0) { 
+    strcat(res, teens[n % 10]);
+  } else if (n != 0) { 
+    strcat(res, ones[n]);
+  }
 }
--- src/test.c
+++ src/test.c
@@ -21,10 +21,104 @@ int failed = 0;
 }
 // End of test helpers
 
-TEST(test_add) {
-  ASSERT(add(1, 2) == 3);
-  ASSERT(add(0, 0) == 0);
-  ASSERT(add(-1, -1) == -2);
+TEST(test_wordinator) {
+  char res[100] = {"\0"}; 
+
+  wordinator(100, res);
+  ASSERT_STR_EQ(res, "one hundred") 
+  strcpy(res, "");
+
+  wordinator(200, res);
+  ASSERT_STR_EQ(res, "two hundred")
+  strcpy(res, "");
+
+  wordinator(120, res);
+  ASSERT_STR_EQ(res, "one hundred and twenty")
+  strcpy(res, "");
+
+  wordinator(115, res);
+  ASSERT_STR_EQ(res, "one hundred and fifteen")
+  strcpy(res, "");
+
+  wordinator(217, res);
+  ASSERT_STR_EQ(res, "two hundred and seventeen")
+  strcpy(res, "");
+
+  wordinator(1000, res);
+  ASSERT_STR_EQ(res, "one thousand")
+  strcpy(res, "");
+
+  wordinator(1600, res);
+  ASSERT_STR_EQ(res, "one thousand six hundred")
+  strcpy(res, "");
+
+  wordinator(1007, res);
+  ASSERT_STR_EQ(res, "one thousand and seven")
+  strcpy(res, "");
+
+  wordinator(1010, res);
+  ASSERT_STR_EQ(res, "one thousand and ten"); 
+  strcpy(res, "");
+
+  wordinator(3517, res);
+  ASSERT_STR_EQ(res, "three thousand five hundred and seventeen"); 
+  strcpy(res, "");
+
+  wordinator(9478, res);
+  ASSERT_STR_EQ(res, "nine thousand four hundred and seventy eight"); 
+  strcpy(res, "");
+
+  wordinator(59, res);
+  ASSERT_STR_EQ(res, "fifty nine"); 
+  strcpy(res, "");
+
+  wordinator(10, res);
+  ASSERT_STR_EQ(res, "ten"); 
+  strcpy(res, "");
+
+  wordinator(4, res);
+  ASSERT_STR_EQ(res, "four"); 
+  strcpy(res, "");
+
+  wordinator(10000, res);
+  ASSERT_STR_EQ(res, "ten thousand"); 
+  strcpy(res, "");
+
+  wordinator(10100, res);
+  ASSERT_STR_EQ(res, "ten thousand one hundred"); 
+  strcpy(res, "");
+
+  wordinator(10007, res);
+  ASSERT_STR_EQ(res, "ten thousand and seven"); 
+  strcpy(res, "");
+   
+  wordinator(30607, res);
+  ASSERT_STR_EQ(res, "thirty thousand six hundred and seven"); 
+  strcpy(res, "");
+
+  wordinator(100000, res);
+  ASSERT_STR_EQ(res, "one hundred thousand"); 
+  strcpy(res, "");
+
+  wordinator(111000, res);
+  ASSERT_STR_EQ(res, "one hundred and eleven thousand"); 
+  strcpy(res, "");
+
+  wordinator(120000, res);
+  ASSERT_STR_EQ(res, "one hundred and twenty thousand"); 
+  strcpy(res, "");
+
+  wordinator(17, res);
+  ASSERT_STR_EQ(res, "seventeen"); 
+  strcpy(res, "");
+  
+  wordinator(956000, res);
+  ASSERT_STR_EQ(res, "nine hundred and fifty six thousand"); 
+  strcpy(res, "");
+
+  wordinator(650123, res);
+  ASSERT_STR_EQ(res, "six hundred and fifty thousand one hundred and twenty three"); 
+  strcpy(res, "");
 }
 
 TEST(test_string_assert) {
@@ -32,8 +126,8 @@ TEST(test_string_assert) {
 }
 
 int main() {
-  // Add a `RUN_TEST` line for each test function
-  RUN_TEST(test_add);
+  // Add a `RUN_TEST` line for each test function 
+  RUN_TEST(test_wordinator);
   RUN_TEST(test_string_assert);
   return failed;
 }
